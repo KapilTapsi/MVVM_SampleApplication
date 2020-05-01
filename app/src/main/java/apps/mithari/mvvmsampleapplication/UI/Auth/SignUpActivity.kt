38bf07db11ce22d926323f799da590a4d2776161
Kapil Tapsi <kapiltapsi@gmail.com>
2020-05-02 00:36:28 +0530

    Blank Activities Added to model the app
    Signed-off-by: Kapil Tapsi <kapiltapsi@gmail.com>

---
+++ app/src/main/java/apps/mithari/mvvmsampleapplication/UI/Auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package apps.mithari.mvvmsampleapplication.UI.Auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import apps.mithari.mvvmsampleapplication.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
