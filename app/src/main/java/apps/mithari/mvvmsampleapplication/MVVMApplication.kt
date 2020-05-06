e17dafdd35a47b8976884daf9e996b726c783456
Kapil Tapsi <kapiltapsi@gmail.com>
2020-05-06 15:12:40 +0530

    Dependency Injection using KoDeIn is implemented
    Signed-off-by: Kapil Tapsi <kapiltapsi@gmail.com>

---
+++ app/src/main/java/apps/mithari/mvvmsampleapplication/MVVMApplication.kt
@@ -0,0 +1,42 @@
+package apps.mithari.mvvmsampleapplication
+
+import android.app.Application
+import apps.mithari.mvvmsampleapplication.data.db.AppDatabase
+import apps.mithari.mvvmsampleapplication.data.network.MyApi
+import apps.mithari.mvvmsampleapplication.data.network.NetworkConnectionInterceptor
+import apps.mithari.mvvmsampleapplication.data.repositories.UserRepository
+import apps.mithari.mvvmsampleapplication.ui.auth.AuthViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+// this is an application class and it needs to be notified to android manifest as well
+class MVVMApplication() : Application(), KodeinAware {
+    //    this class is an application class. Application class are those classes which are instantiated
+//    before anything else in the app. Here we will instantiate all other classes
+//    which we might need to instantiate in app itslef. This is part of dependency injection
+    override val kodein: Kodein = Kodein.lazy {
+        import(androidXModule(this@MVVMApplication))
+
+//        now we are instantiating our classes one by one here.
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
+//      we created singleton of interceptor and passed instance as parameter. this instance parameter is providing context to interceptor
+
+        bind() from singleton { MyApi(instance()) }
+//        here instance means passing interceptor which we defined above
+
+        bind() from singleton { AppDatabase(instance()) }
+//        here instance means myApi instance which we created above
+
+        bind() from singleton { UserRepository(instance(), instance()) }
+//        here instances are myApi and database instances
+
+        bind() from provider { AuthViewModelFactory(instance()) }
+//         here instance means repository. also we didnot want singleton of it.
+    }
+
+}
